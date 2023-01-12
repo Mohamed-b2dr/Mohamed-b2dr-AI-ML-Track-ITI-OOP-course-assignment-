28f5a64fb015f3d4c1ede8b56e57bb5dabe16cf0
Mohamed Badr <55801427+Mohamed-b2dr@users.noreply.github.com>
2023-01-12 16:10:16 +0200

    Add files via upload

---
+++ Day1/Add By Value/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+void Swap (float num1, float num2)
+    {
+        float temp;
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
+int main()
+{
+    float num1, num2;
+    cout << "Enter First Num \n";
+    cin >> num1;
+
+    cout << "Enter Second Num \n";
+    cin >> num2;
+
+    cout << "Before Swap "<< num1 << " "<< num2 <<"\n";
+
+    Swap(num1, num2);
+    cout << "After Swap "<< num1 <<" "<<num2 <<"\n";
+    return 0;
+}
